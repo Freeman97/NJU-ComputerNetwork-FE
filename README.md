2dd554cbbe77e5969603d9f150e1ed35848401e2
Freeman <freemanscut@gmail.com>
2021-01-07 15:29:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# easy_topo
+
+## 框架·库·工具
+- Vue2.0
+- Element-UI
+- @vue/cli
+
+## 功能及使用说明
+
+### 新建节点
+- 从左侧节点库中拖动到右边空白区域
+- ![image](https://raw.githubusercontent.com/laddwong/easy-topo/master/gif/new%20node.gif)
+
+### 连接节点
+- 鼠标右击节点，左键选择连接选项，点击另一个节点完成连接
+- ![image](https://raw.githubusercontent.com/laddwong/easy-topo/master/gif/connect.gif)
+
+### 节点重命名
+- 鼠标右击节点，左键选择重命名，输入新名称
+- ![image](https://raw.githubusercontent.com/laddwong/easy-topo/master/gif/rename.gif)
+
+### 删除节点
+- 鼠标右击节点，左键选择删除节点并确认
+- ![image](https://raw.githubusercontent.com/laddwong/easy-topo/master/gif/delelte%20node.gif)
---
+++ src/data/defaultLinkData.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001443152,
+        startInterface: "fa0/1",
+        startNodeId: 1610000760792
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001435862,
+        startInterface: "fa0/1",
+        startNodeId: 1610001443152
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001435862,
+        startInterface: "fa0/1",
+        startNodeId: 1610000760792
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001441956,
+        startInterface: "fa0/1",
+        startNodeId: 1610000760792
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001435862,
+        startInterface: "fa0/1",
+        startNodeId: 1610001441956
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001441956,
+        startInterface: "fa0/1",
+        startNodeId: 1610001443152
+    },
+    {
+        endInterface: "fa0/1",
+        endNodeId: 1610001471960,
+        startInterface: "fa0/1",
+        startNodeId: 1610001441956
+    }
+]
